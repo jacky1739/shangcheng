570d2ec43ff06ddffc3c346c6b6c8e002adf5114
jacky <jacky123789456@gmail.com>
2022-02-02 19:29:53 +0800

    feat: 新增storage.js

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -3,4 +3,14 @@
  * @author Jacky
  */
 
-
+import config from './../config'
+export default {
+    setItem(key, val) {
+        let storage = window.localStorage.getItem(config.namespace)
+        let obj = { [key]: val } // 這裡用 key 是變數的話必須用中括號包一下，不加中括號的話就會把它當成一個字串，名叫 key
+        window.localStorage.setItem(config.namespace, JSON.stringify(storage))
+    },
+    getItem() {},
+    clearItem() {},
+    clearAll() {},
+}
